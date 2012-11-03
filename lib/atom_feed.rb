1fc597e788442e8cc774c6d11e7ac5e77b6c6e14
John Pignata <john@pignata.com>
2012-11-03 12:34:50 -0400

    ATOM feed tweaks

--- lib/atom_feed.rb
+++ lib/atom_feed.rb
@@ -17,9 +17,9 @@ class AtomFeed
   def build_document
     @builder ||= Nokogiri::XML::Builder.new.tap do |builder|
       builder.feed(document_attributes) do |feed|
-        feed.title   "john pignata :: tx.pignata.com"
-        feed.updated articles.first.date.iso8601
-        feed.id      "http://tx.pignata.com/index.atom"
+        feed.title   "john pignata"
+        feed.updated articles.first.date.to_time.iso8601
+        feed.id      "http://feeds.feedburner.com/jpignata"
         feed.link    href: "http://tx.pignata.com", type: "text/html", rel: "alternate"
         feed.link    href: "http://tx.pignata.com/index.atom", type: "application/atom+xml", rel: "self"
 
@@ -28,15 +28,19 @@ class AtomFeed
             url = "http://tx.pignata.com" + article.permalink
 
             entry.id        url
-            entry.published article.date.iso8601
-            entry.updated   article.date.iso8601
+            entry.published article.date.to_time.iso8601
+            entry.updated   article.date.to_time.iso8601
             entry.link      href: url, type: "text/html", rel: "alternate"
             entry.title     article.title
             entry.summary   article.summary
 
-            entry.content(type: "html") { |content|
+            entry.author do |author|
+              author.name "John Pignata"
+            end
+
+            entry.content(type: "html") do |content|
               content.cdata article.content
-            }
+            end
           end
         end
       end
@@ -46,7 +50,8 @@ class AtomFeed
   def document_attributes
     {
       "xmlns"    => "http://www.w3.org/2005/Atom",
-      "xml:lang" => "en-US"
+      "xml:lang" => "en-US",
+      "xml:base" => "http://tx.pignata.com"
     }
   end
 end
